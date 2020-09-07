5d53cdb73d4ac0ee54747ccaef50124d8def2004
Dmitry Sky <dmitry.sky@icloud.com>
2020-09-07 14:00:46 -0700

    One time memoization, fetch user with the same id only once

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,4 +1,4 @@
-// import _ from 'lodash';
+import _ from 'lodash';
 import jsonPlaceholder from '../apis/jsonPlaceholder';
 
 export const fetchPosts = () => async dispatch => {
@@ -7,17 +7,20 @@ export const fetchPosts = () => async dispatch => {
     dispatch({ type: 'FETCH_POSTS', payload: response.data });
 };
 
-export const fetchUser = (id) => async dispatch => {
-    const response = await jsonPlaceholder.get(`/users/${id}`);
-
-    dispatch({ type: 'FETCH_USER', payload: response.data })
+// With memoization we can only fetch each user one time
+export const fetchUser = (id) => dispatch => {
+  _fetchUser(id, dispatch);
 };
 
 // In old school JS
 // export const fetchUser = function(id) {
-//     return async function(dispatch) {
-//         const response = await jsonPlaceholder.get(`/users/${id}`);
-
-//         dispatch({ type: 'FETCH_USER', payload: response.data });
+//     return function(dispatch) {
+//         _fetchUser(id, dispatch);
 //     };
 // };
+
+const _fetchUser = _.memoize(async (id, dispatch) => {
+    const response = await jsonPlaceholder.get(`/users/${id}`);
+
+    dispatch({ type: 'FETCH_USER', payload: response.data })
+});
