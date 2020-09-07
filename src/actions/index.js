b32994f1c1372b3d7ba09b37e92130e4eaf06b2b
Dmitry Sky <dmitry.sky@icloud.com>
2020-09-07 14:31:58 -0700

    Action creator within action creator, fetch list of posts from within another action creator

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,5 +1,9 @@
-import _ from 'lodash';
+// import _ from 'lodash';
 import jsonPlaceholder from '../apis/jsonPlaceholder';
+
+export const fetchPostsAndUsers = () => async dispatch => {
+    await dispatch(fetchPosts());
+};
 
 export const fetchPosts = () => async dispatch => {
     const response = await jsonPlaceholder.get('/posts');
@@ -7,20 +11,19 @@ export const fetchPosts = () => async dispatch => {
     dispatch({ type: 'FETCH_POSTS', payload: response.data });
 };
 
-// With memoization we can only fetch each user one time
-export const fetchUser = (id) => dispatch => {
-  _fetchUser(id, dispatch);
-};
-
-// In old school JS
-// export const fetchUser = function(id) {
-//     return function(dispatch) {
-//         _fetchUser(id, dispatch);
-//     };
-// };
-
-const _fetchUser = _.memoize(async (id, dispatch) => {
+export const fetchUser = (id) => async dispatch => {
     const response = await jsonPlaceholder.get(`/users/${id}`);
 
     dispatch({ type: 'FETCH_USER', payload: response.data })
-});
+};
+
+// With memoization we can only fetch each user one time
+// export const fetchUser = (id) => dispatch => {
+//   _fetchUser(id, dispatch);
+// };
+
+// const _fetchUser = _.memoize(async (id, dispatch) => {
+//     const response = await jsonPlaceholder.get(`/users/${id}`);
+
+//     dispatch({ type: 'FETCH_USER', payload: response.data })
+// });
